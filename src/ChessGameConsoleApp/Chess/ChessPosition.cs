647863530a7dd3cf0d5881b77d75d2ef87a383e1
Fagner <fagnergeo0@gmail.com>
2024-08-21 12:00:54 -0300

    Creating a chess position class that converts the game board chess number to an matrix number

---
+++ src/ChessGameConsoleApp/Chess/ChessPosition.cs
@@ -0,0 +1,24 @@
+﻿using ChessGameConsoleApp.Board;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGameConsoleApp.Chess;
+
+internal class ChessPosition(char column, int line)
+{
+    public char Column { get; private set; } = column;
+    public int Line { get; private set; } = line;
+    
+    public Position ToPosition()
+    {
+        return new Position(8 - Line, Column - 'a');
+    }
+
+    public override string ToString()
+    {
+        return $"{Column}{Line}";
+    }
+}
--- src/ChessGameConsoleApp/Program.cs
+++ src/ChessGameConsoleApp/Program.cs
@@ -4,16 +4,21 @@ using ChessGameConsoleApp.Board.Enums;
 using ChessGameConsoleApp.Board.Exceptions;
 using ChessGameConsoleApp.Chess;
 
-try
-{
-    GameBoard gameBoard = new(8, 8);
-    gameBoard.PlacePiece(new Tower(Color.White, gameBoard), new Position(0, 0));
-    gameBoard.PlacePiece(new Tower(Color.White, gameBoard), new Position(1, 11));
-    gameBoard.PlacePiece(new King(Color.White, gameBoard), new Position(0, 2));
+ChessPosition chessPosition = new('f', 2);
+Console.WriteLine(chessPosition);
 
-    Display.DisplayGameBoard(gameBoard);
-}
-catch (GameBoardException e)
-{
-    Console.WriteLine(e.Message);
-}
+Console.WriteLine(chessPosition.ToPosition());
+
+//try
+//{
+//    //GameBoard gameBoard = new(8, 8);
+//    //gameBoard.PlacePiece(new Tower(Color.White, gameBoard), new Position(0, 0));
+//    //gameBoard.PlacePiece(new Tower(Color.White, gameBoard), new Position(1, 5));
+//    //gameBoard.PlacePiece(new King(Color.White, gameBoard), new Position(0, 2));
+
+//    //Display.DisplayGameBoard(gameBoard);
+//}
+//catch (GameBoardException e)
+//{
+//    Console.WriteLine(e.Message);
+//}
