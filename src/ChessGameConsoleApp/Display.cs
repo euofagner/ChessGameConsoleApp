4873e373179a3e717e5139420ed5cb4f42dd4934
Fagner <fagnergeo0@gmail.com>
2024-08-21 14:59:59 -0300

    Updating game board display and setting the color yellow to represent the black piece

--- src/ChessGameConsoleApp/Display.cs
+++ src/ChessGameConsoleApp/Display.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using ChessGameConsoleApp.Board;
+using ChessGameConsoleApp.Board.Enums;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,14 +14,33 @@ internal class Display
     {
         for (int i = 0; i < gameBoard.Lines; i++)
         {
+            Console.Write(8 - i + " ");
             for (int j = 0; j < gameBoard.Columns; j++)
             {
                 if (gameBoard.Piece(i, j) == null)
                     Console.Write("- ");
                 else
-                    Console.Write($"{gameBoard.Piece(i, j)} ");
+                {
+                    PrintPiece(gameBoard.Piece(i, j));
+                    Console.Write(" ");
+                }
+                
             }
             Console.WriteLine();
+        }
+        Console.WriteLine("  a b c d e f g h");
+    }
+
+    public static void PrintPiece(Piece piece)
+    {
+        if (piece.Color == Color.White)
+            Console.Write(piece);
+        else
+        {
+            ConsoleColor aux = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(piece);
+            Console.ForegroundColor = aux;
         }
     }
 }
--- src/ChessGameConsoleApp/Program.cs
+++ src/ChessGameConsoleApp/Program.cs
@@ -4,21 +4,17 @@ using ChessGameConsoleApp.Board.Enums;
 using ChessGameConsoleApp.Board.Exceptions;
 using ChessGameConsoleApp.Chess;
 
-ChessPosition chessPosition = new('f', 2);
-Console.WriteLine(chessPosition);
+try
+{
+    GameBoard gameBoard = new(8, 8);
+    gameBoard.PlacePiece(new Tower(Color.White, gameBoard), new Position(0, 0));
+    gameBoard.PlacePiece(new Tower(Color.White, gameBoard), new Position(1, 5));
+    gameBoard.PlacePiece(new King(Color.Black, gameBoard), new Position(0, 2));
+    gameBoard.PlacePiece(new King(Color.Black, gameBoard), new Position(3, 7));
 
-Console.WriteLine(chessPosition.ToPosition());
-
-//try
-//{
-//    //GameBoard gameBoard = new(8, 8);
-//    //gameBoard.PlacePiece(new Tower(Color.White, gameBoard), new Position(0, 0));
-//    //gameBoard.PlacePiece(new Tower(Color.White, gameBoard), new Position(1, 5));
-//    //gameBoard.PlacePiece(new King(Color.White, gameBoard), new Position(0, 2));
-
-//    //Display.DisplayGameBoard(gameBoard);
-//}
-//catch (GameBoardException e)
-//{
-//    Console.WriteLine(e.Message);
-//}
+    Display.DisplayGameBoard(gameBoard);
+}
+catch (GameBoardException e)
+{
+    Console.WriteLine(e.Message);
+}
